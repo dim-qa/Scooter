33638b620e890cc1415cd72e9293b2248afd4078
Dim <habansi@yandex.ru>
2024-08-12 23:13:03 +0500

    tests account and allure after review

--- conftest.py
+++ conftest.py
@@ -2,12 +2,12 @@ import pytest
 import requests
 import data
 import links
-from pages import new_log_pass
+import helper
 
 
 @pytest.fixture()
 def new_account():
-    data_account = new_log_pass.register_new_courier_and_return_login_password()
+    data_account = helper.register_new_courier_and_return_login_password()
     id_account = requests.post(f'{links.MAIN}{data.METHOD_CODE[0]["login"]}', data=data_account)
     id_ele = id_account.json()['id']
     data.account = id_account
